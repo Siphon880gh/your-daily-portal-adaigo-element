679de2707c2fb3457d2ca88aa3cee4fc4a176996
Weng Fei Fung <weffung@ucdavis.edu>
2020-11-28 22:11:35 -0800

    Clean mode has a gray background with less lines and dots, so it's a cleaner look

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -162,7 +162,14 @@ async function render(section) {
                 if (modeSelected === "mode-zen") {
                     $("#particles-js").css("background-color", 'rgb(83, 25, 218)');
                 } else if (modeSelected === "mode-clean") {
+                    // Clean mode is gray background with lesser dots and lines in the background
                     $("#particles-js").css("background-color", "gray");
+                    particlesJS("particles-js", { "particles": { "number": { "value": 40, "density": { "enable": true, "value_area": 900 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 2 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 4, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 600, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 30, "duration": 2, "opacity": 4, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 2 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });
+                    let update;
+                    update = function() {
+                        if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {}
+                        requestAnimationFrame(update);
+                    };
                 } else if (modeSelected === "mode-dark") {
                     $("#particles-js").css("background-color", "black");
                 }
